9a7113d25116969932a52c60e980c7f93b97ed2c
Aridane Martín <aridanemartin@gmail.com>
2023-12-30 20:09:18 +0000

    New sections

---
+++ src/_components/NutritionSections/NutritionSections.tsx
@@ -0,0 +1,47 @@
+'use client'
+
+import './NutritionSections.scss'
+import Image from 'next/image'
+import nutricionPersonalizada from '@assets/pictures/nutricion-personalizada.jpg'
+import nutricionColectividades from '@assets/pictures/nutricion-colectividades.jpg'
+
+export const NutritionSections = () => {
+  return (
+    <section className="sections" id="servicios">
+      <div className="sections__box sections__box1">
+        <h1 className="sections__title">Nutrición Personalizada</h1>
+        <div className="sections__content">
+          <div className="sections__content-description">
+            <h2>Nutrición Personalizada</h2>
+            <p>
+              Asesoramiento Nutricional para todas las etapas de la vida:
+              Infancia, adolescencia, embarazo, edad adulta, menopausia, edad
+              avanzada.
+              <br />
+              <br />
+              Nutrición Clínica para obesidad, diabetes, hipertensión,
+              alteraciones tiroideas, renales digestivas, hepáticas, alergias e
+              intolerancias y otros.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="sections__box sections__box2">
+        <h1 className="sections__title">Nutrición de Colectividades</h1>
+        <div className="sections__content">
+          <div className="sections__content-description">
+            <h2>Nutrición de Colectividades</h2>
+            <p>
+              Elaboración de menús para comedores escolares, centros
+              sociosanitarios y geriátricos de acuerdo a la legislación vigente.
+              <br />
+              <br />
+              Asesoramiento en Seguridad Alimentaria: control higiénico
+              sanitario de las instalaciones.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { getPosts } from 'src/sanity/utils/sanity-querys'
 import { PostPreview } from '@components/PostPreview/PostPreview'
 import Headline from '@components/Headline/Headline'
 import { SectionsPreview } from '@components/SectionsPreview/SectionsPreview'
+import { NutritionSections } from '@components/NutritionSections/NutritionSections'
 
 interface indexProps {}
 
@@ -20,7 +21,7 @@ export default async function Home({}: indexProps) {
           title="Servicios"
           subtitle="Descubre cómo la Nutrición Personalizada puede transformar tu bienestar en cada etapa de la vida. Complementa esto con nuestra Nutrición Clínica, que aborda condiciones como obesidad, diabetes y alergias con un enfoque integral."
         />
-        <SectionsPreview />
+        <NutritionSections />
         <Headline
           title="Últimos Posts"
           subtitle="Mantente al día con las tendencias en nutrición a través de nuestro blog. Descubre recetas innovadoras, consejos expertos y noticias sobre bienestar que te guiarán hacia un estilo de vida más saludable."
