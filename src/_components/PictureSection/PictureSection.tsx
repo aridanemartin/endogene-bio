d47ba8e09686854ce43d5ad5d2f95df9ab3d7a80
Aridane Martín <aridanemartin@gmail.com>
2024-01-17 22:55:35 +0000

    Nutricion Personalizada Section

---
+++ src/_components/PictureSection/PictureSection.tsx
@@ -0,0 +1,32 @@
+import React, { ReactComponentElement } from 'react'
+import './PictureSection.scss'
+import Image, { StaticImageData } from 'next/image'
+
+type PicturePosition = 'left' | 'right'
+
+interface PictureSectionProps {
+  pictureSrc: StaticImageData
+  picturePosition: PicturePosition
+  text: ReactComponentElement<any>
+}
+
+const PictureSection = ({
+  pictureSrc: pictureUrl,
+  picturePosition,
+  text,
+}: PictureSectionProps) => {
+  return (
+    <div className={`picture-section picture-section__${picturePosition}`}>
+      <div className="picture-section__picture-wrapper">
+        <Image
+          className="picture-section__picture"
+          src={pictureUrl}
+          alt="Picture"
+        />
+      </div>
+      <div className="picture-section__text">{text}</div>
+    </div>
+  )
+}
+
+export default PictureSection
