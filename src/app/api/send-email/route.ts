c289bc602e065d2d070ff96ba0d2c1768137bc3d
Aridane Martín <aridanemartin@gmail.com>
2024-03-29 19:28:14 +0000

    Remove resend API Key temporary

--- src/app/api/send-email/route.ts
+++ src/app/api/send-email/route.ts
@@ -1,38 +1,38 @@
-import { Resend } from 'resend'
-import { NextResponse } from 'next/server'
-import GraciasPorContactar from 'src/_emails/gracias-por-contactar'
-import MensajeCarolinaEmail from 'src/_emails/mensaje-carolina'
+// import { Resend } from 'resend'
+// import { NextResponse } from 'next/server'
+// import GraciasPorContactar from 'src/_emails/gracias-por-contactar'
+// import MensajeCarolinaEmail from 'src/_emails/mensaje-carolina'
 
-const resend = new Resend(process.env.NEXT_RESEND_API_KEY)
+// const resend = new Resend(process.env.NEXT_RESEND_API_KEY)
 
-export async function POST(req) {
-  const res = await req.json()
-  const { name, email, message } = res
+// export async function POST(req) {
+//   const res = await req.json()
+//   const { name, email, message } = res
 
-  try {
-    await resend.emails.send({
-      from: 'info@nutricionsaludablelaspalmas.com',
-      to: 'conutricionsaludable@gmail.com',
-      subject: `Consulta Web - ${name}`,
-      react: MensajeCarolinaEmail({
-        name,
-        message,
-        email,
-      }),
-    })
+//   try {
+//     await resend.emails.send({
+//       from: 'info@nutricionsaludablelaspalmas.com',
+//       to: 'conutricionsaludable@gmail.com',
+//       subject: `Consulta Web - ${name}`,
+//       react: MensajeCarolinaEmail({
+//         name,
+//         message,
+//         email,
+//       }),
+//     })
 
-    await resend.emails.send({
-      from: 'info@nutricionsaludablelaspalmas.com',
-      to: email,
-      subject: `${name}, gracias por contactarnos`,
-      react: GraciasPorContactar({
-        name,
-      }),
-    })
+//     await resend.emails.send({
+//       from: 'info@nutricionsaludablelaspalmas.com',
+//       to: email,
+//       subject: `${name}, gracias por contactarnos`,
+//       react: GraciasPorContactar({
+//         name,
+//       }),
+//     })
 
-    return NextResponse.json({ success: true })
-  } catch (error) {
-    console.error('Error sending email:', error)
-    return NextResponse.json({ success: false, error: error.toString() })
-  }
-}
+//     return NextResponse.json({ success: true })
+//   } catch (error) {
+//     console.error('Error sending email:', error)
+//     return NextResponse.json({ success: false, error: error.toString() })
+//   }
+// }
