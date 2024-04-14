9c593535f53ec23d16c58b4e4925c861616278c2
Aridane Martín <aridanemartin@gmail.com>
2024-04-14 20:42:50 +0100

    Remove unused tarifas section / replace imgs

--- src/_components/HeroTemplate/HeroTemplate.tsx
+++ src/_components/HeroTemplate/HeroTemplate.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
-import heroImage from '@assets/pictures/carolinaProfileSquare.webp'
+import heroImage from '@assets/pictures/educationExample.jpg'
 
 import './HeroTemplate.scss'
 import Link from 'next/link'
