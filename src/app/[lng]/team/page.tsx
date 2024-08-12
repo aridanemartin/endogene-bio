43678a66d9f7723161d3dee1bb01be04663f9dc8
Aridane Martín <aridanemartin@gmail.com>
2024-08-12 17:43:27 +0100

    Add rule for browsers not supporting aspect-ratio

--- src/app/[lng]/team/page.tsx
+++ src/app/[lng]/team/page.tsx
@@ -1,13 +1,8 @@
-import Header from '@components/Header/Header'
 import Headline from '@components/Headline/Headline'
-import HeroBannerImage from '@assets/pictures/officeSharing.webp'
-import firstSectionImage from '@assets/pictures/teamPrize.webp'
-import secondSectionImage from '@assets/pictures/teamMeeting.webp'
 import { getTeamMembers } from 'src/sanity/utils/sanity-querys'
 import { TeamSection } from '@components/TeamSection/TeamSection'
 import Layout from '@components/Layout/Layout'
 import '../../../styles/TeamPage.css'
-import PictureSection from '@components/PictureSection/PictureSection'
 
 const TeamPage = async ({ params: { lng } }) => {
   const team = await getTeamMembers()
