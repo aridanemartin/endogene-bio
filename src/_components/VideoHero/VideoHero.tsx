23ca487d3c234af8169e58ab4ceff6afcc9663b2
Aridane Martín <aridanemartin@gmail.com>
2024-08-12 15:25:02 +0100

    Add videos sources / change middleware

--- src/_components/VideoHero/VideoHero.tsx
+++ src/_components/VideoHero/VideoHero.tsx
@@ -19,14 +19,12 @@ export default function VideoHero() {
 
   return (
     <div className="videoHero">
-      <video
-        ref={videoRef}
-        muted
-        autoPlay
-        loop
-        playsInline
-        src={`static/header.mp4`}
-      />
+      <video ref={videoRef} muted autoPlay loop playsInline>
+        <source src={`static/header.mp4`} type="video/mp4" />
+        <source src={`static/header.webm`} type="video/webm" />
+        <source src={`static/header.ogv`} type="video/ogg" />
+      </video>
+
       <div className="videoHero__logo">
         <Image src={logo} alt="logo" />
       </div>
--- src/middleware.ts
+++ src/middleware.ts
@@ -5,36 +5,56 @@ import { fallbackLng, languages, cookieName } from './app/i18n/settings'
 acceptLanguage.languages(languages)
 
 export const config = {
-  // matcher: '/:lng*'
   matcher: [
     '/((?!api|static|_next/static|_next/image|assets|favicon.ico|sw.js).*)',
   ],
 }
 
 export function middleware(req) {
-  let lng
-  if (req.cookies.has(cookieName))
-    lng = acceptLanguage.get(req.cookies.get(cookieName).value)
-  if (!lng) lng = acceptLanguage.get(req.headers.get('Accept-Language'))
-  if (!lng) lng = fallbackLng
+  console.log('Request received:', req.url)
 
-  // Redirect if lng in path is not supported
+  let lng
+
+  // Retrieve language from cookies
+  if (req.cookies.has(cookieName)) {
+    lng = acceptLanguage.get(req.cookies.get(cookieName).value)
+    console.log('Language from cookie:', lng)
+  }
+
+  // Fallback to language from the Accept-Language header
+  if (!lng) {
+    lng = acceptLanguage.get(req.headers.get('Accept-Language'))
+    console.log('Language from Accept-Language header:', lng)
+  }
+
+  // Fallback to the default language
+  if (!lng) {
+    lng = fallbackLng
+    console.log('Fallback language:', lng)
+  }
+
+  // Redirect if language in path is not supported
   if (
     !languages.some((loc) => req.nextUrl.pathname.startsWith(`/${loc}`)) &&
     !req.nextUrl.pathname.startsWith('/_next')
   ) {
+    console.log('Redirecting to:', `/${lng}${req.nextUrl.pathname}`)
     return NextResponse.redirect(
       new URL(`/${lng}${req.nextUrl.pathname}`, req.url),
     )
   }
 
+  // Handle referer for setting the cookie
   if (req.headers.has('referer')) {
     const refererUrl = new URL(req.headers.get('referer'))
     const lngInReferer = languages.find((l) =>
       refererUrl.pathname.startsWith(`/${l}`),
     )
     const response = NextResponse.next()
-    if (lngInReferer) response.cookies.set(cookieName, lngInReferer)
+    if (lngInReferer) {
+      response.cookies.set(cookieName, lngInReferer, { sameSite: 'lax' })
+      console.log('Setting cookie from referer:', lngInReferer)
+    }
     return response
   }
 
