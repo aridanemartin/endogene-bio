08700901993782c27485523d48b8b9bffa6654fb
Aridane Martín <aridanemartin@gmail.com>
2024-08-18 21:55:32 +0100

    Improved blog styling

--- src/_components/BlogPostPreview/BlogPostPreview.tsx
+++ src/_components/BlogPostPreview/BlogPostPreview.tsx
@@ -12,10 +12,14 @@ export const BlogPostPreview = ({ post, index }) => {
   const svgs = [svg1, svg2, svg3, svg4]
   // Determine which SVG to use based on the index
   const selectedSvg = svgs[index % svgs.length]
+  const isOdd = index % 2 === 0
 
   return (
     <Link key={post.slug} href={`blog/${post.slug}`}>
-      <div className="blogPostPreview" key={post._id}>
+      <div
+        className={`blogPostPreview${!isOdd ? ' reverse' : ''}`}
+        key={post._id}
+      >
         <div className="blogPostPreview__image">
           <div className="blogPostPreview__shape">
             <Image src={selectedSvg} alt="Blog shape" />
@@ -27,17 +31,19 @@ export const BlogPostPreview = ({ post, index }) => {
             fill
           />
         </div>
-        <div className="blogPostPreview__title">
-          <h2 id={post.title}>{post.title}</h2>
+        <div className="blogPostPreview__text">
+          <div className="blogPostPreview__title">
+            <h2 id={post.title}>{post.title}</h2>
+          </div>
+          <div
+            className="blogPostPreview__description"
+            aria-labelledby={post.title}
+          >
+            <p className="blogPostPreview__description-text">
+              {post.description}
+            </p>
+          </div>
         </div>
-        <section
-          className="blogPostPreview__description"
-          aria-labelledby={post.title}
-        >
-          <p className="blogPostPreview__description-text">
-            {post.description}
-          </p>
-        </section>
       </div>
     </Link>
   )
