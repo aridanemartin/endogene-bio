635b98b2e5e3f48d274203ce1aba1498862b8ee7
joseignacioendogene <jose.ignacio@endogene.bio>
2026-03-05 13:53:44 +0000

    Add files via upload

--- src/_components/CardSection/CardSection.tsx
+++ src/_components/CardSection/CardSection.tsx
@@ -5,10 +5,12 @@ export const CardSection = ({ t }) => {
     <div className="cardSection">
       <article className="card1 card">
         <div className="text-content">
-          <h2>{t('HOME.our-values.humanity.title')}</h2>
+          {/*<h2>{t('HOME.our-values.humanity.title')}</h2>*/}
+          <h2>{t(' ')}</h2>
           <p
             dangerouslySetInnerHTML={{
-              __html: t('HOME.our-values.humanity.text'),
+            {/*  __html: t('HOME.our-values.humanity.text'),*/}
+              __html: t(' '),
             }}
           />
         </div>
